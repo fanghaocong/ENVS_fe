bf7650aed11dac682fae17a85e344f498f5f62b8
fanghc <imcogncong@gmail.com>
2022-01-22 19:17:45 +0800

    alert

---
+++ src/components/RecordCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrap">
+    <el-card class="record-card">
+      <span>IP地址：{{ recordIP }}，</span>
+      <span>时间： {{ recordDate }}</span>
+    </el-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['record_ip', 'record_date', 'record_path'],
+  data() {
+    return {
+      recordIP: this.record_ip,
+      recordDate: this.record_date,
+      recordPath: this.record_path,
+    }
+  },
+}
+</script>
+
+<style>
+  .wrap {
+    display: flex;
+    justify-content: center;
+  }
+  .record-card {
+    width: 700px;
+    height: 60px;
+  }
+</style>
--- src/views/Alert.vue
+++ src/views/Alert.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="wrap">
-    <h1>This is an alert page</h1>
+  <div class="alert-container">
+    <RecordCardVue v-for="record in records" 
+      v-bind:key="record.sn"
+      :record_ip="record.sn"
+      :record_date="record.record_date"
+      :record_path="record.record_path"></RecordCardVue>
   </div>
 </template>
+
+<script>
+import { CameraAPI } from "@/network/cameraAPI";
+import RecordCardVue from "@/components/RecordCard.vue";
+export default {
+  name: 'Alert',
+  data() {
+    return {
+      records: [],
+    }
+  },
+  components: {
+    RecordCardVue,
+  },
+  created() {
+    let that = this;
+    CameraAPI.getCameras().then(res => {
+      let cameras = res.data.cameras;
+      for (let i = 0; i < cameras.length; i++) {
+        CameraAPI.getCameraRecords(cameras[i].sn).then(res => {
+          let records = res.data.records;
+          for (let j = 0; j < records.length; j++) {
+            that.records.push(records[j]);
+          }
+        })
+      }
+      
+    })
+  }
+}
+</script>
+
+<style>
+  .alert-container {
+    display: grid;
+    grid-template-columns: 700px;
+    grid-template-rows: repeat(auto-fill, 30px);
+    row-gap: 40px;
+    column-gap: 40px;
+    justify-content: center;
+  }
+</style>
